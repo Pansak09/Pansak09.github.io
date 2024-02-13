67f1a01421443438f458489b21626b8c1f258dff
pansak09 <pansak.pa.66@ubu.ac.th>
2024-02-14 01:41:33 +0700

    เพิ่ม app02

--- streamlit/app01.py
+++ streamlit/app01.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
 st.header('My OOP Notebook')
+
+#streamlit run streamlit/app01.py
---
+++ streamlit/app02.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.sidebar.text('Sidebar')
+st.sidebar.button('go>')
+st.sidebar.button('went>')
+st.sidebar.button('gone>')
+st.title('OOP in ML')
+st.image('https://get.pxhere.com/photo/sky-blue-sky-cloud-daytime-atmosphere-horizon-cumulus-air-travel-atmosphere-of-earth-phenomenon-meteorological-phenomenon-flight-calm-sunlight-computer-wallpaper-1435551.jpg')
+st.text_input("What's your name?")
